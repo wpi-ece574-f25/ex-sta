4640f96dbcffa63449b0f3dcdade83781d664e64
Patrick Schaumont <pschaumont@wpi.edu>
2025-09-21 12:05:41 -0400

    update

--- mac/sta/tempus.tcl
+++ mac/sta/tempus.tcl
@@ -6,8 +6,8 @@ set_top_module mac
 read_sdc ../syn/outputs/mac_constraints.sdc
 read_sdf ../syn/outputs/mac_delays.sdf
 
-report_timing -late -max_paths 100 -max_slack 0 > late.rpt
-report_timing -early -max_paths 100 -max_slack 0 > early.rpt
+report_timing -late -max_paths 3 > late.rpt
+report_timing -early -max_paths 3 > early.rpt
 
 report_timing  -from [all_inputs] -to [all_outputs] -max_paths 12 -path_type summary  > allpaths.rpt
 report_timing  -from [all_inputs] -to [all_registers] -max_paths 12 -path_type summary  >> allpaths.rpt
